139b330bc33d5d71e0352e6747e1d61a414005f3
RORY LOUGHRAN - Student <60008783@nrc.ac.uk>
2020-11-03 13:06:04 +0000

    Add project files.

---
+++ projPractical5new/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace projPractical5new
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblFirstNumber = new System.Windows.Forms.Label();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.lblOtherNumber = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(119, 293);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(227, 88);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(456, 293);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(253, 87);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(389, 46);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(161, 20);
+            this.txtOperand1.TabIndex = 2;
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(389, 72);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(161, 20);
+            this.txtOperator.TabIndex = 3;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(389, 98);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(161, 20);
+            this.txtOperand2.TabIndex = 4;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(389, 124);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(161, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // lblFirstNumber
+            // 
+            this.lblFirstNumber.AutoSize = true;
+            this.lblFirstNumber.Location = new System.Drawing.Point(302, 53);
+            this.lblFirstNumber.Name = "lblFirstNumber";
+            this.lblFirstNumber.Size = new System.Drawing.Size(44, 13);
+            this.lblFirstNumber.TabIndex = 6;
+            this.lblFirstNumber.Text = "Number";
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Location = new System.Drawing.Point(302, 79);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(48, 13);
+            this.lblOperator.TabIndex = 7;
+            this.lblOperator.Text = "Operator";
+            // 
+            // lblOtherNumber
+            // 
+            this.lblOtherNumber.AutoSize = true;
+            this.lblOtherNumber.Location = new System.Drawing.Point(302, 105);
+            this.lblOtherNumber.Name = "lblOtherNumber";
+            this.lblOtherNumber.Size = new System.Drawing.Size(73, 13);
+            this.lblOtherNumber.TabIndex = 8;
+            this.lblOtherNumber.Text = "Other Number";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(302, 127);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(37, 13);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblOtherNumber);
+            this.Controls.Add(this.lblOperator);
+            this.Controls.Add(this.lblFirstNumber);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblFirstNumber;
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.Label lblOtherNumber;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ projPractical5new/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projPractical5new
+    //================================================================================================================================================
+    //Program: Practical 5
+    //Date: 12/10/2020
+    //Author: Rory Loughran
+    //Description: This program is a simple calculator that performs simple calculations such as addition, subtraction, multiplication and division.
+    //================================================================================================================================================
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+            private void btnCalculate_Click(object sender, EventArgs e)  //This is the event handler which is started by the users input by clicking the button
+            {
+
+                decimal decResult = 0m;//Initialises variable
+                try
+                {
+                    if (IsValidData())//Checks input data is valid
+                    {
+                        decimal decOperand1 = Convert.ToDecimal(txtOperand1.Text); //This allows the user to enter a number into the textbox and the program  to store as a decimal value
+                        string strOperator = txtOperator.Text; //This allows the user to select the metho that they want to use "+"
+                        decimal decOperand2 = Convert.ToDecimal(txtOperand2.Text); //This allows the user to enter there second number into the textbox and the program  to store as a decimal value
+
+
+                    decResult = Calculate(decOperand1, strOperator, decOperand2); //This calls the calculate function based on the two numbers and the operator passed to the function 
+                        txtResult.Text = Math.Round(decResult, 2).ToString(); //This is used to take the answear from a decimal,2 decimal places, to text
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Contact System Administrator", "User message", MessageBoxButtons.OK, MessageBoxIcon.Warning);//Rounds the result to 2 decimal places and converts it to a string
+                //to display in the text box
+                }
+            }
+
+
+
+
+            private decimal Calculate(decimal decOp1, string strOp, decimal decOp2)//This function is called upon and accepts 3 parameters for the operands and operations
+            {
+                decimal decRes = 0m;//Initialises value
+                switch (strOp)
+                {
+                    case "+":
+                        decRes = decOp1 + decOp2;//If the user enters the + sign then it will add the operands
+                        break;
+                    case "-":
+                        decRes = decOp1 - decOp2;//If the user enters the - sign then it will subtract the operands
+                        break;
+                    case "*":
+                        decRes = decOp1 * decOp2;//If the user enters the * symbol it wil multiply the operands
+                        break;
+                    case "/":
+                        decRes = decOp1 / decOp2;//If the user enters the / Symbol it will divide the operands
+                        break;
+                }
+                return decRes;//Returns result
+            }
+
+            private bool IsDecimal(TextBox txtBox, string text)//This function is called upon and accepts 2 parameters for the text box and the text
+        {
+                bool valid = true;//Initialises value 
+            try
+                {
+                    Convert.ToDecimal(txtBox.Text);//This converts the decimal to text for validation
+                }
+                catch
+                {
+                    valid = false;//Sets valid to false
+                    MessageBox.Show(text + " must be a decimal"); //This is to inform the user that they need to enter a decimal
+                }
+                return valid;//Returns true or false
+            }
+
+
+            private bool IsOperator(TextBox txtBox, string text)//This function is called upon and accepts 2 parameters for the text box and the text
+        {
+                string validOperators;
+                bool valid = true;//Initialises value
+            validOperators = Convert.ToString(txtBox.Text);
+                if (validOperators != "+" && validOperators != "-" && validOperators != "*" && validOperators != "/")//Checks for correct symbol
+                {
+                    MessageBox.Show(text + " must be + - * or /");//This lets the user know that they need to enter an operator
+                    valid = false;//Sets valid to false
+            }
+
+                return valid;//Returns true or false
+        }
+
+            private bool IsPresent(TextBox txtBox, string text)//This function is called upon and accepts 2 parameters for the text box and the text
+        {
+                bool valid = true;//Initialises value
+            if (txtBox.Text == "")
+                {
+                    valid = false;//Sets valid to false
+                MessageBox.Show(text + " is required"); //This is to let them know that they need to enter what ever the system is asking for
+                }
+                return valid;//Returns true or false
+        }
+
+            private bool IsValidData()//Function to call other functions to check if data is valid 
+            {
+                return
+                    IsPresent(txtOperand1, "First operand") && //This is used to remind the user to enter the first number 
+                    IsPresent(txtOperator, "Operator") && //This isused to get the user to select there operator
+                    IsPresent(txtOperand2, "Second operand") && //This is used to get the user to eneter there second number
+
+                    IsDecimal(txtOperand1, "First operand") && //This is used to remind the user that the system requires a decimal not a letter 
+                    IsDecimal(txtOperand2, "Second operand") && //This is used to remind the user that the system requires a decimal not a letter 
+
+                    IsOperator(txtOperator, "Operator");
+            }
+
+
+
+            private void btnExit_Click(object sender, EventArgs e)//This is the event handler which is started by the users input by clicking the button
+        {
+                DialogResult result = MessageBox.Show("Are you sure?", "User message", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);//Message to confirm 
+                if (result == DialogResult.Yes)//IF yes closes form
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
